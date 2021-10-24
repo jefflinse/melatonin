7d1f36a0eaccc0cb31d7d10a4f3836d58f4f6c98
Jeff Linse <jeff@jefflinse.com>
2021-10-24 13:29:37 -0700

    Update example code

--- examples/example_test.go
+++ examples/example_test.go
@@ -11,24 +11,47 @@ import (
 func TestAPI(t *testing.T) {
 	startExampleServer()
 
+	// A custom HTTP request provides complete control over
+	// all aspects of the request when needed.
 	customReq, _ := http.NewRequest("GET", "http://localhost:8080/foo", nil)
 
-	runner := itest.NewTestRunner("http://localhost:8080").WithContinueOnFailure(true)
+	// Create a test runner to configure how the tests are run.
+	runner := itest.NewTestRunner("http://localhost:8080").
+		WithHTTPClient(http.DefaultClient).
+		WithRequestTimeout(time.Second * 5).
+		WithContinueOnFailure(true)
+
 	runner.RunTestsT(t, []*itest.TestCase{
 
 		itest.GET("/foo").
-			WithHeader("Accept", "application/json").
-			WithTimeout(1 * time.Second).
+			WithTimeout(1 * time.Second). // specify a timeout for the test case
 			ExpectStatus(200).
-			ExpectBody(itest.JSONObject{"response": "Hello, world!"}),
+			ExpectBody(itest.String("Hello, world!")),
 
 		itest.GET("/bar?query=foo&other=bar").
 			ExpectStatus(404),
 
-		// Specify a custom *http.Request for a test.
-		// Caller is responsible for constructing the request and ensuring it is valid.
+		itest.POST("/foo").
+			WithHeader("Accept", "application/json"). // add a single header
+			WithBody(itest.JSONObject{                // specify the body using Go types
+				"key": "value",
+			}).
+			ExpectStatus(201),
+
+		itest.POST("/foo").
+			WithHeaders(http.Header{ // set all headers at once
+				"Accept": []string{"application/json"},
+				"Auth":   []string{"Bearer 12345"},
+			}).
+			WithBody(itest.String(`{"key":"value"}`)). // specify body as a string
+			ExpectStatus(201),
+
+		itest.DELETE("/foo").
+			ExpectStatus(204),
+
+		// use any custom *http.Request for a test
 		itest.DO(customReq).
 			ExpectStatus(200).
-			ExpectBody(itest.JSONObject{"response": "Hello, world!"}),
+			ExpectBody(itest.String("Hello, world!")),
 	})
 }
