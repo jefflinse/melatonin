4d00f78bd3a7e6e1b24424fb74724d13e4baba71
Jeff Linse <jeff@jefflinse.com>
2021-10-26 11:55:28 -0700

    Refactor and improve golden file parsing

--- go.mod
+++ go.mod
@@ -4,17 +4,16 @@ go 1.17
 
 require (
 	github.com/fatih/color v1.13.0
+	github.com/spf13/afero v1.6.0
 	github.com/stretchr/testify v1.7.0
+	golang.org/x/text v0.3.3
 )
-
-require golang.org/x/text v0.3.3 // indirect
 
 require (
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.9 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/spf13/afero v1.6.0
 	github.com/stretchr/objx v0.1.0 // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
--- itest/golden.go
+++ itest/golden.go
@@ -18,15 +18,18 @@ type Golden struct {
 	WantStatus  int
 	WantHeaders http.Header
 	WantBody    interface{}
-
-	bodyIsJSON bool
 }
 
-func NewParseGoldenFileFs(fs afero.Fs, path string) (*Golden, error) {
+const (
+	headersLinePrefix = "--- headers"
+	bodyLinePrefix    = "--- body"
+)
+
+func NewGoldenFromFile(fs afero.Fs, path string) (*Golden, error) {
 	if exists, err := afero.Exists(fs, path); err != nil {
 		return nil, err
 	} else if !exists {
-		return nil, fmt.Errorf("golden file %q does not exist", path)
+		return nil, fmt.Errorf("golden file %q: not found", path)
 	}
 
 	f, err := fs.OpenFile(path, os.O_RDONLY, 0)
@@ -36,9 +39,10 @@ func NewParseGoldenFileFs(fs afero.Fs, path string) (*Golden, error) {
 	defer f.Close()
 
 	golden := &Golden{}
-	statusLine := ""
 	var headersLines, bodyLines []string
 	var target *[]string
+	bodyIsJSON := false
+
 	scanner := bufio.NewScanner(f)
 	matcher := search.New(language.English, search.IgnoreCase)
 	for scanner.Scan() {
@@ -49,52 +53,33 @@ func NewParseGoldenFileFs(fs afero.Fs, path string) (*Golden, error) {
 			continue
 		}
 
-		if start, _ := matcher.IndexString(line, "--- headers"); start != -1 {
-			if statusLine == "" {
-				return nil, fmt.Errorf("no status found before encountering headers")
+		// status must be the first non-empty line encountered
+		if golden.WantStatus == 0 {
+			if err := golden.parseStatusLine(line); err != nil {
+				return nil, err
 			}
+			continue
+		}
 
-			headersDirectives := strings.Split(line, " ")
-			for _, directive := range headersDirectives[2:] {
-				directive = strings.TrimSpace(directive)
-				if directive == "" {
-					continue
-				}
-
-				switch directive {
-				default:
-					return nil, fmt.Errorf("unknown headers directive %q", directive)
-				}
+		if start, _ := matcher.IndexString(line, headersLinePrefix); start != -1 {
+			if err := golden.parseHeaderDirectives(line[2:]); err != nil {
+				return nil, err
 			}
 
 			target = &headersLines
 			continue
-		} else if start, _ := matcher.IndexString(line, "--- body"); start != -1 {
-			if statusLine == "" {
-				return nil, fmt.Errorf("no status found before encountering body %q", statusLine)
-			}
-
-			bodyDirectives := strings.Split(line, " ")
-			for _, directive := range bodyDirectives[2:] {
-				directive = strings.TrimSpace(directive)
-				if directive == "" {
-					continue
-				}
-
-				switch directive {
-				case "json":
-					golden.bodyIsJSON = true
-				default:
-					return nil, fmt.Errorf("unknown body directive %q", directive)
-				}
+		} else if start, _ := matcher.IndexString(line, bodyLinePrefix); start != -1 {
+			if bodyIsJSON, err = golden.parseBodyDirectives(line[2:]); err != nil {
+				return nil, err
 			}
 
 			target = &bodyLines
 			continue
-		} else if statusLine == "" {
-			statusLine = line
-			continue
 		} else {
+			if target == nil {
+				return nil, newGoldenFileError(path, fmt.Errorf("unexpected line %q", line))
+			}
+
 			*target = append(*target, line)
 		}
 	}
@@ -103,35 +88,105 @@ func NewParseGoldenFileFs(fs afero.Fs, path string) (*Golden, error) {
 		return nil, err
 	}
 
-	golden.WantStatus, err = strconv.Atoi(statusLine)
-	if err != nil {
-		return nil, fmt.Errorf("invalid status %q: %s", golden.WantStatus, err)
+	if err := golden.parseHeaderLines(headersLines); err != nil {
+		return nil, err
 	}
 
-	if len(headersLines) > 0 {
-		golden.WantHeaders = http.Header{}
-		for _, line := range headersLines {
-			parts := strings.SplitN(line, ":", 2)
-			if len(parts) != 2 {
-				return nil, fmt.Errorf("invalid header %q", line)
-			}
-
-			key := strings.TrimSpace(parts[0])
-			value := strings.TrimSpace(parts[1])
-			golden.WantHeaders.Add(key, value)
-		}
-	}
-
-	if len(bodyLines) > 0 {
-		body := strings.Join(bodyLines, "\n")
-		if golden.bodyIsJSON {
-			if err := json.Unmarshal([]byte(body), &golden.WantBody); err != nil {
-				return nil, fmt.Errorf("invalid JSON body %q: %s", body, err)
-			}
-		} else {
-			golden.WantBody = body
-		}
+	if err := golden.parseBodyLines(bodyLines, bodyIsJSON); err != nil {
+		return nil, err
 	}
 
 	return golden, nil
+}
+
+func (g *Golden) parseStatusLine(line string) error {
+	var err error
+	g.WantStatus, err = strconv.Atoi(line)
+	if err != nil {
+		return fmt.Errorf("invalid status %q", line)
+	}
+
+	return nil
+}
+
+func (g *Golden) parseHeaderDirectives(line string) error {
+	headersDirectives := strings.Split(line, " ")
+	for _, directive := range headersDirectives[2:] {
+		directive = strings.TrimSpace(directive)
+		if directive == "" {
+			continue
+		}
+
+		switch directive {
+		default:
+			return fmt.Errorf("unknown headers directive %q", directive)
+		}
+	}
+
+	return nil
+}
+
+func (g *Golden) parseHeaderLines(lines []string) error {
+	if len(lines) > 0 {
+		g.WantHeaders = http.Header{}
+		for _, line := range lines {
+			if line == "" {
+				continue
+			}
+
+			parts := strings.SplitN(line, ":", 2)
+			if len(parts) != 2 {
+				return fmt.Errorf("invalid header %q", line)
+			}
+
+			key := strings.TrimSpace(parts[0])
+			if key == "" {
+				return fmt.Errorf("invalid header key %q", line)
+			}
+
+			value := strings.TrimSpace(parts[1])
+
+			g.WantHeaders.Add(key, value)
+		}
+	}
+
+	return nil
+}
+
+func (g *Golden) parseBodyDirectives(line string) (bool, error) {
+	bodyDirectives := strings.Split(line, " ")
+	for _, directive := range bodyDirectives[2:] {
+		directive = strings.TrimSpace(directive)
+		if directive == "" {
+			continue
+		}
+
+		switch directive {
+		case "json":
+			return true, nil
+		default:
+			return false, fmt.Errorf("unknown body directive %q", directive)
+		}
+	}
+
+	return false, nil
+}
+
+func (g *Golden) parseBodyLines(lines []string, asJSON bool) error {
+	if len(lines) > 0 {
+		body := strings.Join(lines, "\n")
+		if asJSON {
+			if err := json.Unmarshal([]byte(body), &g.WantBody); err != nil {
+				return fmt.Errorf("invalid JSON body: %s\n---\n%s\n---", err, body)
+			}
+		} else {
+			g.WantBody = body
+		}
+	}
+
+	return nil
+}
+
+func newGoldenFileError(path string, err error) error {
+	return fmt.Errorf("golden file %q: %w", path, err)
 }
