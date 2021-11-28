5075fc66417452d5b74ee23f77b07d466c02efaf
Jeff Linse <jeff@jefflinse.com>
2021-11-27 22:01:50 -0800

    Rearrange output code

--- mt/output.go
+++ mt/output.go
@@ -19,34 +19,6 @@ var (
 	blueBG  = color.New(color.BgBlue, color.FgHiWhite).SprintFunc()
 )
 
-type columnWriter struct {
-	columns   int
-	format    string
-	dest      io.Writer
-	tabWriter *tabwriter.Writer
-}
-
-func newColumnWriter(output io.Writer, columns int, padding int) *columnWriter {
-	return &columnWriter{
-		columns:   columns,
-		format:    strings.Repeat("%s\t", columns) + "\n",
-		dest:      output,
-		tabWriter: tabwriter.NewWriter(output, 0, 0, padding, ' ', 0),
-	}
-}
-
-func (w *columnWriter) printLine(str string, args ...interface{}) {
-	fmt.Fprintf(w.tabWriter, str+"\n", args...)
-}
-
-func (w *columnWriter) printColumns(columns ...interface{}) {
-	if len(columns) > w.columns {
-		panic(fmt.Sprintf("PrintColumns() called with %d columns, expected at most %d", len(columns), w.columns))
-	}
-
-	fmt.Fprintf(w.tabWriter, w.format, columns...)
-}
-
 func PrintRunResult(result RunResult) {
 	w := newColumnWriter(cfg.Stdout, 5, 2)
 
@@ -68,6 +40,43 @@ func PrintRunResult(result RunResult) {
 	w.tabWriter.Flush()
 }
 
+type columnWriter struct {
+	columns   int
+	format    string
+	dest      io.Writer
+	tabWriter *tabwriter.Writer
+}
+
+func newColumnWriter(output io.Writer, columns int, padding int) *columnWriter {
+	return &columnWriter{
+		columns:   columns,
+		format:    strings.Repeat("%s\t", columns) + "\n",
+		dest:      output,
+		tabWriter: tabwriter.NewWriter(output, 0, 0, padding, ' ', 0),
+	}
+}
+
+func (w *columnWriter) printColumns(columns ...interface{}) {
+	if len(columns) > w.columns {
+		panic(fmt.Sprintf("PrintColumns() called with %d columns, expected at most %d", len(columns), w.columns))
+	}
+
+	fmt.Fprintf(w.tabWriter, w.format, columns...)
+}
+
+func (w *columnWriter) printLine(str string, args ...interface{}) {
+	fmt.Fprintf(w.tabWriter, str+"\n", args...)
+}
+
+func (w *columnWriter) printTestSuccess(testNum int, result TestResult, duration time.Duration) {
+	w.printColumns(
+		greenFG(fmt.Sprintf("✔ %d", testNum)),
+		whiteFG(result.TestCase().Description()),
+		blueBG(fmt.Sprintf("%7s ", result.TestCase().Action())),
+		result.TestCase().Target(),
+		faintFG(duration.String()))
+}
+
 func (w *columnWriter) printTestFailure(testNum int, result TestResult, duration time.Duration) {
 	w.printColumns(
 		redFG(fmt.Sprintf("✘ %d", testNum)),
@@ -79,13 +88,4 @@ func (w *columnWriter) printTestFailure(testNum int, result TestResult, duration
 	for _, err := range result.Failures() {
 		w.printColumns(redFG(""), redFG(fmt.Sprintf("  %s", err)), blueBG(""), "", faintFG(""))
 	}
-}
-
-func (w *columnWriter) printTestSuccess(testNum int, result TestResult, duration time.Duration) {
-	w.printColumns(
-		greenFG(fmt.Sprintf("✔ %d", testNum)),
-		whiteFG(result.TestCase().Description()),
-		blueBG(fmt.Sprintf("%7s ", result.TestCase().Action())),
-		result.TestCase().Target(),
-		faintFG(duration.String()))
 }
